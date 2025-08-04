5d2f86645d3fce00a3667dcc78e4be79c94ec500
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-08-04 17:59:15 -0500

    changed layout and added/modified classes related to layout elements

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -3,11 +3,13 @@ import { database } from './database.js'
 
 export const cookiesList = () => {
     // Generate an HTML representation of each cookie card
-    return database.cookies.map(cookie => 
+    return database.cookies.map(cookie =>
         `<div class="cookie-card">
-            <h3>${cookie.name}!</h3>
-            <p>${cookie.description}</p>
-            <img src= "${cookie.img}" alt="${cookie.name} image" class="cookie__image">
+          <img src= "${cookie.img}" alt="${cookie.name} image" class="cookie-card__image">
+            <div class="cookie-card__text">
+                <h3 class="cookie-card__name">${cookie.name}!</h3>
+                <p class="cookie-card__description">${cookie.description}</p>
+            </div>
         </div>`
     ).join("")
 }
